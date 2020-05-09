28f5e7a2ab828406f4faeb5c3bb5f8ca44d36c35
aakashHirnotia <ahirnotia@ch.iitr.ac.in>
2020-05-09 11:58:42 +0530

    Updates

---
+++ precache-manifest.349481392a7e9b8ae6a0e9b044725674.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d851e97be139f0922ce980e1c435b52b",
+    "url": "/carRental/index.html"
+  },
+  {
+    "revision": "424b69f518fe70f632e5",
+    "url": "/carRental/static/css/main.8604ba9c.chunk.css"
+  },
+  {
+    "revision": "d26efdffdf3eb360da60",
+    "url": "/carRental/static/js/2.7a527f3a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/carRental/static/js/2.7a527f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424b69f518fe70f632e5",
+    "url": "/carRental/static/js/main.6d2f9c01.chunk.js"
+  },
+  {
+    "revision": "84c28af6ce7f93d5dd69",
+    "url": "/carRental/static/js/runtime-main.44b02b0a.js"
+  },
+  {
+    "revision": "0053f3d8fba4ea70906ef706c4d268d3",
+    "url": "/carRental/static/media/1maruti.0053f3d8.jpg"
+  },
+  {
+    "revision": "8a6b58afd0dcf0076b1c614a3006d302",
+    "url": "/carRental/static/media/images.8a6b58af.png"
+  }
+]);
